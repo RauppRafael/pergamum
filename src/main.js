9cbd7ae673e1128906372e5445bc87c97b18b0f0
Rafael Raupp <raupperino@gmail.com>
2018-05-11 19:25:44 -0300

    Adding bootstrap-select and font-awesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import general js
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// import general css
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 new Vue({
   el: '#app',
