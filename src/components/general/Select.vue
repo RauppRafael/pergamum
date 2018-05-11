4d7781f639d487750cc2bb46515f727f47a18fcb
Rafael Raupp <raupperino@gmail.com>
2018-05-11 20:07:41 -0300

    Adding orderBy

---
+++ src/components/general/Select.vue
@@ -0,0 +1,110 @@
+<template>
+  <select ref="select" :class="dataClass" :disabled="dataDisabled">
+    <slot></slot>
+  </select>
+</template>
+
+<script>
+  import 'bootstrap/dist/js/bootstrap.min.js'
+  import 'bootstrap-select/dist/css/bootstrap-select.min.css'
+  import 'bootstrap-select/dist/js/bootstrap-select.min.js'
+
+  export default {
+
+    props: {
+      dataWidth: {required: false},
+      dataSelected: {required: false},
+      dataSearch: {required: false, default: true, type: Boolean},
+      dataClass: {required: false, default: ''},
+      dataDisabled: {required: false, default: false},
+    },
+
+    data() {
+      return {
+        select: null
+      }
+    },
+
+    computed: {
+
+      width() {
+        return this.dataWidth || 'auto'
+      },
+
+      selected() {
+        return this.dataSelected || ''
+      },
+    },
+
+    methods: {
+
+      init() {
+        this.select = $(this.$refs.select)
+
+        this.select.selectpicker({
+          width: this.width,
+          liveSearch: this.dataSearch,
+        })
+
+        this.select.on('changed.bs.select', (e) => {
+          this.propagate()
+        })
+
+        this.setValue(this.selected)
+      },
+
+      setValue(v) {
+        this.select.selectpicker('val', v)
+      },
+
+      render() {
+        this.select.selectpicker('render')
+        this.select.selectpicker('refresh')
+      },
+
+      propagate() {
+        this.$emit('update', this.select.val())
+      },
+
+    },
+
+    mounted() {
+      this.init()
+    },
+
+    watch: {
+
+      dataSelected(o) {
+        this.setValue(o)
+      },
+
+      dataDisabled(o) {
+        this.dataDisabled = o
+        this.$nextTick(() => this.render())
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .bootstrap-select.show > .dropdown-menu > .dropdown-menu {
+    display: block;
+  }
+
+  .bootstrap-select > .dropdown-menu > .dropdown-menu li.hidden {
+    display: none;
+  }
+
+  .bootstrap-select > .dropdown-menu > .dropdown-menu li a {
+    display: block;
+    width: 100%;
+    padding: 3px 1.5rem;
+    clear: both;
+    font-weight: 400;
+    color: #292b2c;
+    text-align: inherit;
+    white-space: nowrap;
+    background: 0 0;
+    border: 0;
+  }
+</style>
--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="container">
     <div class="card">
+
       <div class="card-header">
         <h2>Pesquisa Geral</h2>
       </div>
+
       <div class="card-body">
+
+        <div class="row">
+
+          <div class="col-md-6">
+            <label for="">&nbsp;</label>
+            <div class="form-group">
+              <input type="text" class="form-control">
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <label>Ordenação</label>
+            <app-select data-search="false">
+              <option value="">Titulo</option>
+              <option value="">Ano de publicação</option>
+              <option value="">Código de acervo</option>
+              <option value="">Tipo de obra</option>
+              <option value="">Idioma</option>
+              <option value="">Descrição física</option>
+            </app-select>
+          </div>
+
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import AppSelect from '../general/Select'
+  export default {
+    components: {
+      AppSelect
+    }
+  }
 </script>
 
 <style scoped>
