c63d0af1e083dad31d6a1601ac20353eee323a3d
Rafael Raupp <raupperino@gmail.com>
2018-06-06 21:26:16 -0300

    Adding vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import store from './store'
+
 // import general js
 import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -9,7 +11,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
+// import store from './store'
+
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
