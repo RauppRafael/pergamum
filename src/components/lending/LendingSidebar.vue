5bf4a009bd5312f839fb94b1fe149f8dfad58bbe
Rafael Raupp <raupperino@gmail.com>
2018-06-14 22:36:44 -0300

    Adding lending sidebar

--- src/components/lending/LendingSidebar.vue
+++ src/components/lending/LendingSidebar.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
+    <div>
+      <h6>Empréstimo</h6>
+      <ul>
+        <li><a href="javascript:void(0)">Renovação</a></li>
+        <li><a href="javascript:void(0)">Reserva</a></li>
+        <li><a href="javascript:void(0)">Débito</a></li>
+        <li><a href="javascript:void(0)">Histórico</a></li>
+        <li><a href="javascript:void(0)">Declaração de nada consta</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h6>Perfil de interesses</h6>
+      <ul>
+        <li><a href="javascript:void(0)">Areas de interesse</a></li>
+        <li><a href="javascript:void(0)">Cesta permanente</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h6>Solicitação</h6>
+      <ul>
+        <li><a href="javascript:void(0)">Consulta ao malote</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h6>Aquisição</h6>
+      <ul>
+        <li><a href="javascript:void(0)">Consultar sugestões</a></li>
+      </ul>
+    </div>
 
   </div>
 </template>
